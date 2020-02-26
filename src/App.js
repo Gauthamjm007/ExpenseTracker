8c93591973566987128b89e889fd6f1a5d134a0d
Gauthamjm007 <j.mgautham15@gmail.com>
2020-02-26 20:30:13 +0530

    adding a few components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpense from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header title="Expense Tracker" />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
