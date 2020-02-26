27353f12989db9ccb80746917ada3c3768b4b733
Gauthamjm007 <j.mgautham15@gmail.com>
2020-02-26 23:12:45 +0530

    Updates

---
+++ precache-manifest.de289bf445b5e22653aa5ec4a54899cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52415a6fd86fa429d5afae9731559d96",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "ddcbbbcb662c37ef6090",
+    "url": "/ExpenseTracker/static/css/main.117e2a82.chunk.css"
+  },
+  {
+    "revision": "eeae4a973fb52cc50d5e",
+    "url": "/ExpenseTracker/static/js/2.e238c0c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ExpenseTracker/static/js/2.e238c0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddcbbbcb662c37ef6090",
+    "url": "/ExpenseTracker/static/js/main.cc003ac5.chunk.js"
+  },
+  {
+    "revision": "93e2beac9051f55c8218",
+    "url": "/ExpenseTracker/static/js/runtime-main.01888dec.js"
+  }
+]);
